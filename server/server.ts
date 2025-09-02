53e466709738e295e0f4ecda5ca5c087a31fb03c
Anuragpandey26 <anuragkumarpandey2001@gmail.com>
2025-09-02 20:31:54 +0530

    just the commit

--- server/server.ts
+++ server/server.ts
@@ -28,7 +28,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("/{*splat}", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
